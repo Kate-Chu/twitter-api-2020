c09bfd3954bcd95e7b60c8c87e39f12d1dde7dae
SeijoHuang <erinhuang817@gmail.com>
2022-05-10 14:05:25 +0800

    add popular html

---
+++ src/components/Popular.vue
@@ -0,0 +1,35 @@
+<template>
+  <main id="popular">
+    <h1>跟隨誰</h1>
+    <div class="popular__list">
+      <div class="popular__list__item">
+        <img src="./../assets/static/images/noimage@2x.png" alt="">
+        <div class="item-account">
+          <p class="name">Pizza Hut</p>
+          <small class="id">@pizzahut</small>
+        </div>
+        <button class="following">正在跟隨</button>
+        <button class="follow">跟隨</button>
+      </div>    
+    </div> 
+  </main>
+</template>
+<style lang="scss">
+  @import "../assets/scss/_basic.scss";
+  main{
+    width:273px;
+    height:731px;
+    background: #FAFAFB;
+    border-radius: 16px;
+    h1{
+      border-bottom: 1px solid #E6ECF0;
+      padding: 24px;
+    }
+    .popular__list__item{   
+      display: flex;
+      padding: 1rem;
+      
+    } 
+  }
+
+</style>
