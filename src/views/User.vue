10674de4075640a904f2b02be6acd2458134db4c
SeijoHuang <erinhuang817@gmail.com>
2022-05-11 01:43:12 +0800

    add User page with Sidebar and Popular

---
+++ src/views/User.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <div class="container">
+      <div class="row">
+        <div class="col-3 sidebar">
+      <Sidebar />
+      </div>
+      <div class="col-6 content-container">
+
+      </div>
+      <div class="col-3  popular-users">
+        <Popular />
+      </div>
+      </div>      
+    </div>  
+  </main>
+</template>
+<script>
+  import Popular from '../components/Popular.vue'
+  import Sidebar from '../components/Sidebar.vue'
+  export default {
+    components:{
+      Popular,
+      Sidebar
+    }      
+  }
+</script>
